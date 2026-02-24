79fb1dea3aec7fc4457fcaff5cc1d07e207a796c
OneMoreHope <natham.chatsuwan@g.swu.ac.th>
2026-02-24 11:50:24 +0700

    complete laew mung

--- webdemo/models/expense.js
+++ webdemo/models/expense.js
@@ -24,22 +24,21 @@ class ExpenseModel {
     this.expenses = [];
   }
 
-  /** 
-   * Add an expense object to the array.
+  /** * Add an expense object to the array.
    * @param {Expense} expense - An Expense object.
   */
   add(expense) {
     this.expenses.push(expense);
   }
 
-/**
- * return expense numbers from stack.
- * @return {array} expense - array of positive number
- */
-
+  /**
+   * return expense numbers from stack.
+   * @return {array} expense - array of positive number
+   */
   getAll() {
     return this.expenses;
   }
+
   /**
    * Calculate the total income.
    * @return {number} The sum of all income.
@@ -47,14 +46,15 @@ class ExpenseModel {
   getTotalIncome() {
     return this.expenses.reduce((sum, exp) => sum + exp.income, 0);
   }
+
   /**
    * Calculate the total expense.
    * @return {number} The sum of all expenses.
   */
-
   getTotalExpense() {
     return this.expenses.reduce((sum, exp) => sum + exp.expense, 0);
   }
+
   /**
    * Calculate the money left (Total Income - Total Expense).
    * @return {number} The remaining money.
